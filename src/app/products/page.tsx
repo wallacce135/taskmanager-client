71cb079ea9e54100b62def7e929d7f0bf79ab440
kga-tech <kga.tech@lidera.agency>
2024-05-21 21:17:40 +0300

    project initialization

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next";
+
+
+
+
+const ProductsPage: NextPage = () => {
+    return(
+        <>
+        <h2>Products</h2>
+        </>
+    )
+}
+
+export default ProductsPage;
